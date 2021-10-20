6fc6c42e3dd9f27d1d0be2ff3efb77b48252cd8d
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2021-10-20 17:44:45 +0900

    Add: serviceテーブルへのデータ投入

---
+++ db/fixtures/development/01_service.rb
@@ -0,0 +1,14 @@
+Service.seed(
+  {
+    id: 1,
+    name: 'マッスルデリ',
+    body: '高タンパク質の宅配食サービス',
+    service_url: 'https://muscledeli.co.jp/'
+  },
+ {
+    id: 2,
+    name: '筋肉食堂DELI',
+    body: 'カラダづくりを志す全ての人たちへの宅配食サービス',
+    service_url: 'https://kinnikushokudo-ec.jp/'
+  },
+)
