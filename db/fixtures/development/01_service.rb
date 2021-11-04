a3131d8d85fdc3bb972196f2ad55d1cf67f6ffef
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2021-11-04 14:13:54 +0900

    Add: serviceページの詳細

--- db/fixtures/development/01_service.rb
+++ db/fixtures/development/01_service.rb
@@ -2,13 +2,15 @@ Service.seed(
   {
     id: 1,
     name: 'マッスルデリ',
-    body: '高タンパク質の宅配食サービス',
-    service_url: 'https://muscledeli.co.jp/'
+    body: '製作者愛用の高タンパク質の宅配食サービス',
+    service_url: 'https://muscledeli.co.jp/',
+    service_image: 'muscledeli.png'
   },
   {
     id: 2,
     name: '筋肉食堂DELI',
     body: 'カラダづくりを志す全ての人たちへの宅配食サービス',
-    service_url: 'https://kinnikushokudo-ec.jp/'
+    service_url: 'https://kinnikushokudo-ec.jp/',
+    service_image: 'kinniku.jpg'
   }
 )
