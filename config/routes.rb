eaea7eea032df85aa76c4ec183b6ffd6ea140d7b
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2021-11-10 14:42:00 +0900

    Add: menu 一覧ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  resources :services, only: [:index, :show]
+  resources :services, only: [:index, :show] do
+    resources :menus, only: [:index]
+  end
 end
