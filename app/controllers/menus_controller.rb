567e275845fb105d5bfba38c9f64d036059ce695
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2021-11-15 14:56:25 +0900

    Add: all_menusページ

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,8 @@
 class MenusController < ApplicationController
+  def all_menus
+    @menus = Menu.all
+  end
+  
   def index
     @service = Service.find(params[:service_id])
     @menus = Menu.where(service_id: @service)
--- db/fixtures/development/02_menu.rb
+++ db/fixtures/development/02_menu.rb
@@ -20,5 +20,25 @@ Menu.seed(
     menu_image: 'menu02.jpg',
     size: 'LEAN',
     service_id: 1
+  },
+  {
+    id: 3,
+    name: '牛ヒレステーキ（ペッパー）×牛赤身低糖質ハンバーグ（キノコ）',
+    calorie: 558,
+    protein: 42.9,
+    fat: 24.1,
+    carb: 41.9,
+    menu_image: 'kinniku01.png',
+    service_id: 2
+  },
+  {
+    id: 4,
+    name: '牛赤身肉のステーキ（韓国風）×牛赤身低糖質ハンバーグ（ジンジャー）',
+    calorie: 557,
+    protein: 41,
+    fat: 22.2,
+    carb: 46.9,
+    menu_image: 'kinniku02.png',
+    service_id: 2
   }
 )
