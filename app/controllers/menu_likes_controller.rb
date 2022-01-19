82f6c5914ff84464dfc45c4726df558d34a4115f
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2022-01-19 22:40:13 +0900

    Add: いいね機能Ajax化

--- app/controllers/menu_likes_controller.rb
+++ app/controllers/menu_likes_controller.rb
@@ -2,14 +2,12 @@ class MenuLikesController < ApplicationController
   before_action :require_login
 
   def create
-    menu = Menu.find(params[:menu_id])
-    current_user.menu_like(menu)
-    redirect_back fallback_location: root_path, success: 'いいねしました'
+    @menu = Menu.find(params[:menu_id])
+    current_user.menu_like(@menu)
   end
 
   def destroy
-    menu = current_user.menu_likes.find(params[:id]).menu
-    current_user.menu_unlike(menu)
-    redirect_back fallback_location: root_path, success: 'いいねを取り消しました'
+    @menu = current_user.menu_likes.find(params[:id]).menu
+    current_user.menu_unlike(@menu)
   end
 end
--- app/controllers/review_likes_controller.rb
+++ app/controllers/review_likes_controller.rb
@@ -2,14 +2,12 @@ class ReviewLikesController < ApplicationController
   before_action :require_login
 
   def create
-    review = Review.find(params[:review_id])
-    current_user.review_like(review)
-    redirect_back fallback_location: root_path, success: 'いいねしました'
+    @review = Review.find(params[:review_id])
+    current_user.review_like(@review)
   end
 
   def destroy
-    review = current_user.review_likes.find(params[:id]).review
-    current_user.review_unlike(review)
-    redirect_back fallback_location: root_path, success: 'いいねを取り消しました'
+    @review = current_user.review_likes.find(params[:id]).review
+    current_user.review_unlike(@review)
   end
 end
