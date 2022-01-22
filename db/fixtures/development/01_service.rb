8f63ab687fee016a6f57fc5c4976c39afd9a2924
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2022-01-22 13:32:02 +0900

    Add: serviceデータ追加

--- db/fixtures/development/01_service.rb
+++ db/fixtures/development/01_service.rb
@@ -1,16 +1,58 @@
 Service.seed(
   {
     id: 1,
-    name: 'マッスルデリ',
-    body: '製作者愛用の高タンパク質の宅配食サービス',
+    name: 'Muscle Deli',
+    body: '目的に合わせたサイズや、豊富なメニューを誇る高タンパク質宅配食サービス',
     service_url: 'https://muscledeli.co.jp/',
-    service_image: 'muscledeli.png'
+    service_image: 'muscledeli.jpg'
   },
   {
     id: 2,
     name: '筋肉食堂DELI',
-    body: 'カラダづくりを志す全ての人たちへの宅配食サービス',
+    body: 'レストラン「筋肉食堂」のお店の味そのままの、美味しい高タンパク低カロリー料理を提供',
     service_url: 'https://kinnikushokudo-ec.jp/',
-    service_image: 'kinniku.jpg'
+    service_image: 'kinnikushokudou.jpg'
+  },
+  {
+    id: 3,
+    name: 'BIRUMESHI',
+    body: 'パーソナルトレーナー＆管理栄養士が全工程を監修した高タンパク宅配食',
+    service_url: 'https://birumeshi.com/',
+    service_image: 'birumeshi.jpg'
+  },
+  {
+    id: 4,
+    name: 'GOFOOD',
+    body: '本格シェフが美味しさを追求し、健康食を研究する医師も認めた宅配食',
+    service_url: 'https://gofood.jp/shop',
+    service_image: 'gofood.jpg'
+  },
+  {
+    id: 5,
+    name: 'nosh-ナッシュ',
+    body: '管理栄養士とシェフが監修した糖質と塩分に配慮したメニューを提供するサービス',
+    service_url: 'https://nosh.jp/',
+    service_image: 'nosh.png'
+  },
+  {
+    id: 6,
+    name: 'B-Kitchen',
+    body: 'フィットネスジム監修の栄養素の元、管理栄養士がメニューを作成し、美味しい仕出し弁当会社の手作りをお届けする宅配食',
+    service_url: 'https://kitchen.db-mall.jp/',
+    service_image: 'b-kitchen.jpg'
+  },
+  {
+    id: 7,
+    name: 'ニチレイ パワーデリ',
+    body: 'ニチレイの衛生管理の徹底された国内工場で丁寧に製造された、高タンパクな食事を楽しめる宅配食',
+    service_url: 'https://wellness.nichirei.co.jp/shop/c/c1015/',
+    service_image: 'nitirei.jpg'
+  },
+  {
+    id: 8,
+    name: 'MORE THAN DELI',
+    body: '”我慢”する食事管理ではなく、”楽しみ”になる食事管理を目指してつくられた高タンパク低カロリー宅配食',
+    service_url: 'https://morethandeli.co.jp/',
+    service_image: 'morethandeli.jpg'
   }
 )
