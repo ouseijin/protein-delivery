b031b0d0739eb84c83723aebbadce92e9a1918af
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2022-01-27 20:09:25 +0900

    Fix: menu表示ページのN+1問題解決

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,16 +1,15 @@
 class MenusController < ApplicationController
   def all_menus
     @q = Menu.ransack(params[:q])
-    @menus = @q.result(distinct: true).includes(:service, :menu_likes)
+    @menus = @q.result(distinct: true).includes(:service, :nutrients, :menu_likes)
   end
 
   def index
-    # @service = Service.find(params[:service_id])
     @menus = Menu.where(service_id: params[:service_id]).includes(:service, :nutrients, :menu_likes)
-    # @nutrients = Nutrient.where(menu_id: @menus)
   end
 
   def menu_rank
-    @menu_like_ranks = Menu.find(MenuLike.group(:menu_id).order('count(menu_id) desc').limit(5).pluck(:menu_id))
+    @menu_like_ranks = Menu.includes(:service, :nutrients, :menu_likes)
+                           .find(MenuLike.group(:menu_id).order('count(menu_id) desc').limit(5).pluck(:menu_id))
   end
 end
