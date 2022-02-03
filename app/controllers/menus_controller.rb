be035e43be820c3d3dc9d9a3b64628b1257179a8
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2022-02-03 21:36:42 +0900

    Add: menu表示にページネーション機能追加

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,11 +1,12 @@
 class MenusController < ApplicationController
   def all_menus
     @q = Menu.ransack(params[:q])
-    @menus = @q.result(distinct: true).includes(:service, :nutrients, :menu_likes)
+    @menus = @q.result(distinct: true).includes(:service, :nutrients, :menu_likes).page(params[:page])
   end
 
   def index
-    @menus = Menu.where(service_id: params[:service_id]).includes(:service, :nutrients, :menu_likes)
+    @service = Service.find(params[:service_id])
+    @menus = Menu.where(service_id: params[:service_id]).includes(:service, :nutrients, :menu_likes).page(params[:page])
   end
 
   def menu_rank
