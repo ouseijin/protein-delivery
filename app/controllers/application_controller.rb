f3140b7f7d4fc21233484a600f96f62279a3483b
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2022-03-06 20:04:49 +0900

    Add: 未ログイン時のログイン誘導確認画面実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
   private
 
   def not_authenticated
-    redirect_to login_path, info: 'こちらは、ログインしている場合に使える機能です。ぜひ、ログインして使って下さい。'
+    redirect_to login_path, info: 'こちらは、ログイン画面です。　SNS認証もございますので、ぜひお使い下さい。'
   end
+
+  def create_alert_data
+    if logged_in?
+      data = nil
+    else
+      data = { confirm: "【確認】こちらはログインした方のみが使える機能となります。\nOKを押して頂ければログイン画面に移動します。"}
+    end
+  end
+  helper_method :create_alert_data
 end
