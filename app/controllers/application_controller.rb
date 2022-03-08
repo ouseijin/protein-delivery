a0e5d1ce8182b5abe8b38042d22e6d01764e1b73
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2022-03-08 11:46:26 +0900

    Fix: viewで扱うメソッドをhelperに移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,4 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     redirect_to login_path, info: 'こちらは、ログインページです。　SNS認証もございますので、ぜひお使い下さい。'
   end
-
-  def create_alert_data
-    if logged_in?
-      data = nil
-    else
-      data = { confirm: "【確認】こちらはログインした方のみが使える機能となります。\nログインページへ移動しますか？",
-        cancel: 'キャンセル',
-        commit: 'ログインページへ移動する'
-      }
-    end
-  end
-  helper_method :create_alert_data
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def create_alert_data
+    if logged_in?
+      nil
+    else
+      {
+        confirm: "【確認】こちらはログインした方のみが使える機能となります。\nログインページへ移動しますか？",
+        cancel: 'キャンセル',
+        commit: 'ログインページへ移動する',
+        title: 'ログインページへの案内'
+      }
+    end
+  end
 end
