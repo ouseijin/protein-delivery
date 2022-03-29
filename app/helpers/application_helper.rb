8968654edcd624046bb52ddbf9dd29cf1f0c3c13
Riskjunk <colofulcooke@gmail.com>
2022-03-29 19:23:24 +0900

    Add: Metaタグの設定追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,33 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'protein-delivery',
+      title: '身体づくりに励むあなたへの高タンパク宅配食レビューサイト',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'protein-deliveryは高タンパクな宅配食専門のレビューサイトです。皆で良かった商品の情報を提供し合いましょう！',
+      keywords: 'たんぱく質,筋トレ,ダイエット,宅配食,ボディメイク,口コミ,protein,diet',
+      canonical: 'https://www.protein-delivery.net',
+      separator: '|',
+      icon: [
+        { href: image_url('icon.png') }
+      ],
+      og: {
+        site_name: 'protein-delivery',
+        title: '身体づくりに励むあなたへの高タンパク宅配食レビューサイト',
+        description: 'protein-deliveryは高タンパクな宅配食専門のレビューサイトです。皆で良かった商品の情報を提供し合いましょう！',
+        type: 'website',
+        url: 'https://www.protein-delivery.net',
+        image: image_url('ogp.jpg'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@aJ2q8qRROxPbesk'
+      }
+    }
+  end
+
   def create_alert_data
     if logged_in?
       nil
